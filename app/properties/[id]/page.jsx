fd752448059319a3c3e0cd001768c6ea2e12aff8
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2025-01-20 22:15:17 +0200

    adding routing

---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PropertyPage = () => {
+    return (
+        <div>
+            hi from single property page MF!
+        </div>
+    );
+};
+
+export default PropertyPage;
