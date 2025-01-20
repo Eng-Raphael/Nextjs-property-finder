7b135583f91498e365bd37160721df9a68a33307
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2025-01-20 18:55:29 +0200

    adding meta data and images

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+export const metadata = {
+    title:'Property Finder',
+    keywords: 'property, finder, real estate rental buy sell',
+    description: 'Find your perfect rental property or home to buy in the UK',
+}
+
 const HomePage = () => {
     return (
         <div className='text-2xl'>
