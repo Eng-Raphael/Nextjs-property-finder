2c52f4120130837ae8a6bfa49b9d38ff3c7e7cbb
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2025-01-20 18:15:19 +0200

    adding layout and home page

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className='text-2xl'>
+            Home page
+        </div>
+    );
+};
+
+export default HomePage;
